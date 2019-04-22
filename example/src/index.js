3ae26ad0f5e447af213cb28744718f650dfdf4ab
sakal <sakals@mail.ua>
2019-04-22 18:37:13 -0400

    Initial commit

---
+++ example/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {render} from 'react-dom';
+import './style.css';
+
+//import Cube from '../../dist';
+
+function App() {
+  return (
+      <h1>foo</h1>
+  );
+}
+
+render(<App/>, document.getElementById('root'));
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import './style.css';
+
+function Foo() {
+
+}
+
+export default Foo;
