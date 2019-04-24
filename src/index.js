5d0c50f87f3922564cba1c99564023b5d5608bd8
sakal <sakals@mail.ua>
2019-04-24 18:12:57 -0400

    feat: new parameter 'direction' for choosing corner of start animation
    Signed-off-by: sakal <sakals@mail.ua>

--- src/index.js
+++ src/index.js
@@ -2,41 +2,90 @@ import React, {useEffect, useRef} from 'react';
 
 import './style.css';
 
-function animate(elStyling, type = 'out') {
+function animate({
+                   direction = 'top-right',
+                   elStyling,
+                   isOpen,
+                 }) {
+  let translateX,
+      translateXReverse,
+      translateOverlay,
+      skewCorner,
+      skewCornerReverse;
 
-  let translateX = '-170vw';
-  let translateXReverse = '170vw';
-  let translateOverlay = '340vw';
+  // Choosing direction:
+  switch (direction) {
+    case 'top-left':
+      translateX = '-170vw';
+      translateXReverse = '170vw';
+      translateOverlay = '340vw';
+      skewCorner = '-20deg';
+      skewCornerReverse = '20deg';
+      break;
+    case 'top-right':
+      translateX = '170vw';
+      translateXReverse = '-170vw';
+      translateOverlay = '-340vw';
+      skewCorner = '20deg';
+      skewCornerReverse = '-20deg';
+      break;
+    case 'bottom-left':
+      translateX = '-170vw';
+      translateXReverse = '170vw';
+      translateOverlay = '340vw';
+      skewCorner = '20deg';
+      skewCornerReverse = '-20deg';
+      break;
+    case 'bottom-right':
+      translateX = '170vw';
+      translateXReverse = '-170vw';
+      translateOverlay = '-340vw';
+      skewCorner = '-20deg';
+      skewCornerReverse = '20deg';
+      break;
+  }
 
-  if (type === 'in') {
+  if (isOpen) {
     translateOverlay = translateXReverse = translateX = '0';
   }
 
   elStyling.setProperty('--translate-x', translateX);
   elStyling.setProperty('--translate-x-reverse', translateXReverse);
   elStyling.setProperty('--translate-x-overlay', translateOverlay);
+  elStyling.setProperty('--skew', skewCorner);
+  elStyling.setProperty('--skew-reverse', skewCornerReverse);
 }
 
-function OverlayEffect({isOpen, children}) {
+function OverlayEffect({
+                         children,
+                         direction,
+                         isOpen,
+                       }) {
   const containerEl = useRef(null);
 
   useEffect(() => {
-
     const containerStyling = containerEl.current.style;
 
-    (isOpen)
-        ? animate(containerStyling, 'in')
-        : animate(containerStyling, 'out');
+    animate({
+      direction,
+      elStyling: containerStyling,
+      isOpen,
+    });
+
   }, [isOpen]);
+
+  useEffect(() => {
+    const containerStyling = containerEl.current.style;
+
+    setTimeout(() => {
+      containerStyling.setProperty('top', 0);
+    }, 500);
+  }, []);
 
   return (
       <div className='-overlay-container' ref={containerEl}>
         <div className='-overlay-slide-skew'>
-
-          <section className='-overlay-slide-2'>
-
-
-          </section>
+          <section className='-overlay-bg-screen'/>
 
           <div className='-overlay-reverse-skew'>
             <div className='-overlay-reverse-slide'>
